4c01490867138dfd8cd45d048cbc775118e13570
vivin-erialis <vivineriali@gmail.com>
2024-06-10 15:45:21 +0700

    update

---
+++ .history/resources/views/Frontend/artikel/header-artikel.blade_20240610153200.php
@@ -0,0 +1,12 @@
+<div class="container-fluid page-header-1 text-center mt-2 py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <h6 class="display-1 text-white animated slideInDown" style="font-size: 5vw !important">{{$headerStart}}</h6>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb text-uppercase mb-0" style="justify-content: center">
+                <li class="breadcrumb-item"><a class="text-white" href="/">Home</a></li>
+                <li class="breadcrumb-item"><a class="text-white" href="/artikel">Artikel</a></li>
+                <li class="breadcrumb-item"><a class="text-white" href="/berita">Berita</a></li>
+            </ol>
+        </nav>
+    </div>
+</div>
