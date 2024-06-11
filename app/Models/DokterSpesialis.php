c105a9533ea5c38c8057549d227ff5754ef9c93a
mulyadirizki <mulyadirizkiputra10@gmail.com>
2024-06-11 16:49:54 +0700

    update

---
+++ app/Models/DokterSpesialis.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DokterSpesialis extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_dokter_spesialis';
+
+    protected $primaryKey = 'id_dokter_spesialis';
+
+    protected $fillable = [
+        'id_dokter_spesialis',
+        'id_dokter',
+        'id_spesialis',
+        'idhafis',
+        'statusenabled'
+    ];
+}
