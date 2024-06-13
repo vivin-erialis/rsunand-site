d16160a9d8142a87372f4e88827693ee0cac6b5b
mulyadirizki <mulyadirizkiputra10@gmail.com>
2024-06-13 08:32:55 +0700

    update

--- resources/views/Frontend/layouts/navbar.blade.php
+++ resources/views/Frontend/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
     <a href="/" class="navbar-brand ms-4 ms-lg-0 d-flex mt-1">
-        <img class="me-3" style="width: 50px; height: 60px;" src="/../assets/img/rsunandlogo.png" alt="Icon">
-        <h3 class="text-primary mt-2 ">RS UNIVERISTAS <br> ANDALAS</h3>
+        <img class="me-3" style="width: 40px; height: 50px;" src="/../assets/img/rsunandlogo.png" alt="Icon">
+        <h3 class="text-primary mt-1 ">Rumah Sakit <br> Univeristas Andalas</h3>
     </a>
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
