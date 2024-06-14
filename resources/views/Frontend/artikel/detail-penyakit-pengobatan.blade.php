1533e8f284705dddff170f9622f93a361fb852d7
mulyadirizki <mulyadirizkiputra10@gmail.com>
2024-06-14 12:05:39 +0700

    Update

--- resources/views/Frontend/artikel/detail-penyakit-pengobatan.blade.php
+++ resources/views/Frontend/artikel/detail-penyakit-pengobatan.blade.php
@@ -8,24 +8,24 @@
         <div class="">
             <div class="p-5" style="font-size: 15px !important;">
             @php
-    // Decode the JSON containing the image file names
-    $gambar = json_decode($penyakitPengobatan->gambar, true);
+                // Decode the JSON containing the image file names
+                $gambar = json_decode($penyakitPengobatan->gambar, true);
 
-    // Split the content into paragraphs
-    $paragraf = explode("</p>", $penyakitPengobatan->isi);
-@endphp
+                // Split the content into paragraphs
+                $paragraf = explode("</p>", $penyakitPengobatan->isi);
+            @endphp
 
-<!-- Display the first image as the cover if it exists -->
-@if (!empty($gambar) && isset($gambar[0]))
-    <div class="cover-image mb-4">
-        <img loading="lazy" class="img-fluid" style="width: 100%;" src="{{ asset('/../images/artikel/' . $gambar[0]) }}" alt="Cover Image">
-    </div>
-@endif
+            <!-- Display the first image as the cover if it exists -->
+            @if (!empty($gambar) && isset($gambar[0]))
+                <div class="cover-image mb-4">
+                    <img loading="lazy" class="img-fluid" style="width: 100%;" src="{{ asset('/../images/artikel/' . $gambar[0]) }}" alt="Cover Image">
+                </div>
+            @endif
 
-<!-- Display the content paragraphs -->
-@foreach ($paragraf as $paragrafItem)
-    <p>{!! $paragrafItem !!}</p>
-@endforeach
+            <!-- Display the content paragraphs -->
+            @foreach ($paragraf as $paragrafItem)
+                <p>{!! $paragrafItem !!}</p>
+            @endforeach
 
             </div>
             <div class="mt-2 px-5">
